652b4c3b9d1ed98595afd947435cd2d783ccfb89
Kent D <kent.daleng@nav.no>
2023-08-08 23:08:07 +0200

    return empty list on liveinfo for now

--- src/app/api/players.ts
+++ src/app/api/players.ts
@@ -2,9 +2,9 @@ import { OnlineUser, User } from "@/types/players";
 import { getApi } from "./instance";
 
 export async function getCurrentPlayersFromAPI(): Promise<OnlineUser[]> {
-    const api = await getApi();
-    const response = await api.get("/liveinfo"); // TODO: correct url whenever the endpoint exists
-    return response.data;
+    // const api = await getApi();
+    // const response = await api.get("/liveinfo"); // TODO: correct url whenever the endpoint exists
+    return [];
 }
 
 export async function getPlayerFromAPI(userId: string): Promise<User> {
