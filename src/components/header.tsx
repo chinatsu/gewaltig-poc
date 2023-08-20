dbf388adc67f6113b8f72ff9f33f4890f922ea58
Kent D <kent.daleng@nav.no>
2023-08-20 12:13:05 +0200

    add now playing to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,6 +5,7 @@ export function Header() {
     return <header className="bg-slate-700 w-screen p-4 flex gap-4 items-center text-lg">
         <Image src="/images/gw.png" alt="GEWALTIG entertainment" height="0" width="0" sizes="100vw" className="h-12 w-auto" />
         <Link href="https://gewaltig.net/" className="text-lime-300 hover:text-lime-600 transition-all">Play Cultris II</Link>
+        <Link href="/" className="text-white hover:text-blue-600 transition-all">Now playing</Link>
         <Link href="/achievements" className="text-white hover:text-blue-600 transition-all">Achievements</Link>
     </header>
 }
